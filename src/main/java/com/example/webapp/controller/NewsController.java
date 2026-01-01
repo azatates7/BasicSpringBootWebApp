9db8e819bc53db81044480216d315a823f6213de
azatates7 <azatates4977@gmail.com>
2026-01-01 23:25:05 +0300

    Kurlar Eklendi.

---
+++ src/main/java/com/example/webapp/controller/NewsController.java
@@ -0,0 +1,19 @@
+package com.example.webapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class NewsController {
+
+    @GetMapping("/api/news")
+    public Object getNews() {
+        String apiKey = "47ba5d35dc2f407793573a2ac8216a17";
+        // Google Haberler'den İngilizce en popüler 5 haberi çekiyoruz
+        String url = "https://newsapi.org/v2/top-headlines?sources=google-news&pageSize=5&apiKey=" + apiKey;
+
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, Object.class);
+    }
+}
